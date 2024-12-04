89cff74407304963ed57ab1cd324d0f9e3f6ec5c
Caleb Kiptoo <vincentcalebkip@gmail.com>
2024-12-04 12:07:45 +0300

    create filters

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(drivers, el){
+    return drivers.filter(driver => driver.startsWith(el));
+}
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name);
+
+}
